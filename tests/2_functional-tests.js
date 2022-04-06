5fc74a7c69ddb93f60a85aa4f04edd6edc6f2d3a
ApplianceJohn <thednbgremlin@gmail.com>
2022-04-06 12:14:24 -0400

    updated functional test 1 and 2

--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -14,8 +14,8 @@ suite("Functional Tests", function () {
 			chai.request(server)
 				.get("/hello")
 				.end(function (err, res) {
-					assert.fail(res.status, 200);
-					assert.fail(res.text, "hello Guest");
+					assert.equal(res.status, 200);
+					assert.equal(res.text, "hello Guest");
 					done();
 				});
 		});
@@ -24,8 +24,8 @@ suite("Functional Tests", function () {
 			chai.request(server)
 				.get("/hello?name=xy_z")
 				.end(function (err, res) {
-					assert.fail(res.status, 200);
-					assert.fail(res.text, "hello xy_z");
+					assert.equal(res.status, 200);
+					assert.equal(res.text, "hello xy_z");
 					done();
 				});
 		});
